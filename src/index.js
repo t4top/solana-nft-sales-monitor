8f19190c94039c62bc61f6015bdd09f32fdf8147
t4top <temitope@yahoo.co.jp>
2022-05-08 22:40:15 +0000

    Call NFT sales monitor loop based on configuration settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
+import { readFileSync, existsSync } from "fs";
+import { NFTSalesMonitor } from "./lib/nft_sales_monitor.js";
+import { randomSleep } from "./lib/helper/sleep.js";
+
+const CONFIG_FILE = "config.json";
+
+const readConfig = () =>
+  existsSync(CONFIG_FILE) ? JSON.parse(readFileSync(CONFIG_FILE, "utf-8")) : [];
+
+const runNFTSalesMonitor = async () => {
+  const collections = readConfig();
+
+  if (!(collections && collections.length))
+    return console.log(`No collection found. Make sure ${CONFIG_FILE} is properly configured.`);
+
+  for (let i = 0; i < collections.length; i++) {
+    const monitor = new NFTSalesMonitor(collections[i]);
+    monitor.resume();
+    // insert 10s to 60s random delay between requests not to spam Solana RPC
+    await randomSleep(10000, 60000);
+  }
+};
+
 const main = () => {
-  console.log("main executed.");
+  runNFTSalesMonitor();
 };
 
 main();
