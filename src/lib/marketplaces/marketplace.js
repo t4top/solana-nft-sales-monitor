93a5e23ecb131fcb88a7404233d899fce52dd50a
t4top <temitope@yahoo.co.jp>
2022-05-12 22:14:40 +0000

    Rework logic for finding sales price, buyer and seller

--- src/lib/marketplaces/marketplace.js
+++ src/lib/marketplaces/marketplace.js
@@ -20,9 +20,23 @@ export class NFTMarketplace {
   }
 
   async parseTxn(txn) {
-    const buyer = this.findBuyer(txn);
-    const [seller, salesPrice] = this.findSellerAndSalesPrice(txn, buyer);
-    const mint = this.getTokenMint(txn);
+    let [mint, buyer] = this.findTokenMintAndBuyer(txn);
+    // return if the transaction is not an NFT sale
+    if (!mint) return;
+
+    // if buyer is not found, use the signer instead
+    buyer = buyer || this.findSigner(txn) || "";
+
+    const seller = this.findSeller(txn);
+
+    // return if buyer & seller are the same (i.e NFT delist)
+    if (buyer === seller) return;
+
+    const salesPrice = this.calculateSalesPrice(txn, buyer);
+
+    // return if sales price can't be correctly calculated
+    if (salesPrice <= 0) return;
+
     const metadata = await getNFTMetadata(mainnetConn, mint);
     const mintUrl = this.getItemURL(mint);
 
@@ -40,47 +54,105 @@ export class NFTMarketplace {
     };
   }
 
-  // the buyer is the signer of the transaction
-  findBuyer(txn) {
-    const { accountKeys } = txn.transaction.message;
-    const signer = accountKeys.find(acct => acct.signer);
-    return signer ? signer.pubkey.toBase58() : "";
+  findTokenMintAndBuyer(txn) {
+    const { preTokenBalances, postTokenBalances } = txn.meta;
+
+    let balances = [];
+    if (postTokenBalances?.length) balances = postTokenBalances;
+    else balances = preTokenBalances;
+
+    // a NFT token has a decimal of 0 and an amount of 1
+    const token = balances?.find(
+      el => el.uiTokenAmount.decimals === 0 && el.uiTokenAmount.uiAmount === 1
+    );
+
+    // the buyer receives the NFT after the transaction
+    let buyer = undefined;
+    if (postTokenBalances?.length) buyer = token?.owner;
+
+    return [token?.mint, buyer];
   }
 
-  // the assumption is,
-  // the seller gets the highest SOL payout but not more than the sale price
-  findSellerAndSalesPrice(txn, buyer) {
-    let salesPrice = 0;
-    let seller = "";
-    let sellerPay = 0;
-    let transfers = [];
+  // the first signer can be assummed to be the buyer
+  findSigner(txn) {
+    const { accountKeys } = txn.transaction.message;
+    const signer = accountKeys?.find(acct => acct.signer);
+    return signer?.pubkey?.toBase58();
+  }
 
+  // the seller gets the highest SOL payout
+  findSeller(txn) {
+    const { preBalances, postBalances } = txn.meta;
+    const { accountKeys } = txn.transaction.message;
+    let diff = 0;
+    let maxDiff = 0;
+    let maxIndex = 0;
+
+    for (let i = 0; i < postBalances.length; i++) {
+      diff = postBalances[i] - preBalances[i];
+      if (diff > maxDiff) {
+        maxIndex = i;
+        maxDiff = diff;
+      }
+    }
+
+    return accountKeys[maxIndex].pubkey?.toBase58();
+  }
+
+  // Each marketplace process price differently in the smart contract depending on the type of sales
+  // like regular sales, bid sale, accept offer, auction, etc.
+  calculateSalesPrice(txn, buyer) {
+    const salesPrice =
+      this.priceFromInnerInstructions(txn, buyer) ||
+      this.priceFromMsgInstructions(txn, buyer) ||
+      this.priceFromBalances(txn, buyer);
+
+    return salesPrice / LAMPORTS_PER_SOL;
+  }
+
+  // calculate price from InnerInstructions. This works for regular sales in most markeplaces.
+  priceFromInnerInstructions(txn, buyer) {
     const { innerInstructions } = txn.meta;
+    let listPrice = 0;
+    let bidPrice = 0;
 
     innerInstructions.forEach(el => {
       el.instructions.forEach(ins => {
-        if (ins.parsed && ins.parsed.type === "transfer") {
-          const { destination, lamports, source } = ins.parsed.info;
-          if (source === buyer) salesPrice += lamports;
-          transfers.push({ destination, lamports });
+        if (ins.parsed?.type === "transfer") {
+          const { lamports, source } = ins.parsed.info;
+          if (source === buyer) listPrice += lamports || 0;
+          if (source !== buyer) bidPrice += lamports || 0;
         }
       });
     });
 
-    transfers.forEach(({ destination, lamports }) => {
-      if (salesPrice > lamports && lamports > sellerPay) {
-        sellerPay = lamports;
-        seller = destination;
+    return listPrice || bidPrice;
+  }
+
+  // calculate price from message instructions. This works for transactions done in Wrapped SOL.
+  priceFromMsgInstructions(txn, buyer) {
+    const { instructions } = txn.transaction.message;
+    let listPrice = 0;
+    let bidPrice = 0;
+
+    instructions.forEach(ins => {
+      if (ins.parsed?.type === "transfer") {
+        const { lamports, source } = ins.parsed.info;
+        if (source === buyer) listPrice += lamports || 0;
+        if (source !== buyer) bidPrice += lamports || 0;
       }
     });
 
-    return [seller, salesPrice / LAMPORTS_PER_SOL];
+    return listPrice || bidPrice;
   }
 
-  getTokenMint(txn) {
-    const { preTokenBalances, postTokenBalances } = txn.meta;
+  // calculate price from balances of the buyer. This works if other methods fail.
+  // Trasaction fees are included in sales price.
+  priceFromBalances(txn, buyer) {
+    const { preBalances, postBalances } = txn.meta;
+    const { accountKeys } = txn.transaction.message;
+    const buyerIndex = accountKeys.findIndex(accnt => accnt.pubkey?.toBase58() === buyer);
 
-    if (postTokenBalances && postTokenBalances.length) return postTokenBalances[0].mint;
-    if (preTokenBalances && preTokenBalances.length) return preTokenBalances[0].mint;
+    return preBalances[buyerIndex] - postBalances[buyerIndex];
   }
 }
